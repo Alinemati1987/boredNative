f45b02ce750384d8df3f1c6a9ab3588338d1d530
Alinemati1987 <89942725+Alinemati1987@users.noreply.github.com>
2021-12-07 13:59:32 +0100

    Initialize app - Button fetches activity - image is displayed - the button change by condition

---
+++ store/reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import activitiesReducer from "./activities/reducer";
+// import weather from "./weather/reducer";
+
+const reducer = combineReducers({
+  activities: activitiesReducer,
+  // weather,
+});
+
+export default reducer;
